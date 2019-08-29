21754fb770e9035420c82a96f4facbf6234e275c
victor-ferraz7 <victor.fzn7@gmail.com>
2019-08-29 19:00:08 -0300

    Era Pokemon, agora Digimon

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {  BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+import { string } from 'prop-types'
 
 import { DetalhesPage } from './pages/detalhes';
 import { HomePage } from './pages/home';
-import { NaoEncontradoPage } from './pages/naoEncontrado'
+import { NaoEncontradoPage } from './pages/naoEncontrado';
+import { AppBar, Button, Toolbar, Typography, Container } from '@material-ui/core';
 
 function App(props) {
+
+
   return (
     <BrowserRouter>
-      <Switch>
-        <Route component={HomePage} exact path='/'></Route>
-        <Route component={DetalhesPage} path='/detalhes'></Route>
-        <Route component={NaoEncontradoPage}></Route>
-      </Switch>
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit" component={Link} to="/">
+            <Typography variant="h6">Home</Typography>
+          </Button>
+          <Button color="inherit" component={Link} to="/detalhes">
+            <Typography variant="h6">Detalhes</Typography>
+          </Button>
+        </Toolbar>
+
+      </AppBar>
+      <Container>
+        <Switch>
+          <Route component={HomePage} exact path='/'></Route>
+          <Route component={DetalhesPage} path='/detalhes'></Route>
+          <Route component={NaoEncontradoPage}></Route>
+        </Switch>
+      </Container>
     </BrowserRouter>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card, CardContent, CardMedia, Typography } from '@material-ui/core'
 
+function Digimon(props) {
+  return (
+    <Card elevation={2} style={{display: 'inline-block', margin: '25px', width: '20%', textAlign: 'center'}} >
+      <CardContent>
+        <CardMedia component={'img'} src={props.img} alt={`Imagem do digimon ${props.name}`} />
+        <Typography gutterBottom variant='h5'>{props.name}</Typography>
+        <Typography variant="body2" color="textSecondary">Level: {props.level}</Typography>
+      </CardContent>
+    </Card>
+  )
+}
 
 export function HomePage() {
-    return <h1>Oi, sou uma home Page</h1>
+
+  const [data, setData] = useState([])
+  console.log('data antes do use effects:', data)
+
+  useEffect(function () {
+    fetch('https://digimon-api.herokuapp.com/api/digimon')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        console.log("Resultado da requisição:", response)
+        setData(response);
+      })
+      .catch(function (error) {
+        console.error("error", error)
+      })
+
+  }, []);
+  console.log("DAta depois do useEfects", data)
+  if (data.length > 0) {
+    return (
+      <div>
+        {data.map(function (digimon) {
+          return <Digimon key={digimon.name} {...digimon} />
+        })}
+      </div>
+    )
   }
+  else {
+    return <p>Carregando ...</p>
+  }
+}
